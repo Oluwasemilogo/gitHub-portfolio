c7b6bc6a0fd47d4f661d3db87b9c939f7c19d43a
Semilogo Bamidele <emmanbamidele471@gmail.com>
2023-03-07 13:12:05 +0100

    profile section completed.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# altschool third semester project
- vue projecr
+# altschool-third-semester-project
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="profile-container">
+    <img :src= "data.avatar_url" alt="profile-picture" class="avatar"/> 
+    <h1>{{ data.name }}</h1>
+    <h3 className="username">@{{ data.login }}</h3>
+    <p className="bio">{{ data.bio }}</p>
+    <section class="section">
+      <div class="stat">
+        <p class="profile-figures">{{ data.public_repos }}</p>
+        <h3 class="profile-details">Repositories</h3>
+      </div>
+      <div class="stat">
+        <p class="profile-figures">{{ data.followers }}</p>
+        <h3 class="profile-details">Followers</h3>
+      </div>
+      <div class="stat">
+        <p class="profile-figures">{{ data.following }}</p>
+        <h3 class="profile-details">Following</h3>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    fetch("https://api.github.com/users/Oluwasemilogo", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.data = data;
+      });
+  },
+};
+</script>
+
+<style>
+/* .avatar {
+  width: 140px;
+  height: 140px;
+  border-radius: 50%;
+  margin-bottom: 20px;
+  border: 5px solid rgb(0, 112, 243);
+} */
+</style>
